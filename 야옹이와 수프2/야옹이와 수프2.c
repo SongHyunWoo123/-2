b345dd38e74d96e6f54c364933a825cb4f8d020f
SongHyunWoo123 <thdgusdn118@naver.com>
2025-06-05 14:43:52 +0900

    2-1 기본 상태 변수들 추가

---
+++ 야옹이와 수프2/야옹이와 수프2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void) {
+	int soupCount = 0;     // 만든 수프 수
+    int cp = 0;            // Cute Point
+    int mood = 3;          // 기분 (0~3)
+    int bond = 2;          // 집사와의 관계도 (0~4)
+    int hasScratcher = 0;  // 스크래처 보유 여부 (0: 없음, 1: 있음)
+    int hasCatTower = 0;   // 캣타워 보유 여부 (0: 없음, 1: 있음)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
